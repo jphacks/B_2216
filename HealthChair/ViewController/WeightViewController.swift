8cf3d05d4cd3bb0baf00c6026589c0974eae8a09
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2022-10-19 01:09:12 +0900

    feature: linechartの修正

--- HealthChair/ViewController/WeightViewController.swift
+++ HealthChair/ViewController/WeightViewController.swift
@@ -19,14 +19,37 @@ class WeightViewController: UIViewController {
     }
     
     func setUpGraph(){
-        let rawData: [Int] = [20, 50, 70, 30, 60, 90, 40]
+        let rawData: [Int] = [72, 74, 70, 69, 68, 70, 71]
         let entries = rawData.enumerated().map { BarChartDataEntry(x: Double($0.offset), y: Double($0.element)) }
         let dataSet = LineChartDataSet(entries: entries)
         dataSet.drawValuesEnabled = false
         dataSet.colors = [.systemOrange]
+        dataSet.drawCirclesEnabled = false
+        dataSet.lineWidth = 5
+        dataSet.mode = .linear
         
         let data = LineChartData(dataSet: dataSet)
         lineChartView.data = data
+        
+        lineChartView.setScaleEnabled(false)
+        lineChartView.highlightPerDragEnabled = false
+        lineChartView.highlightPerTapEnabled = false
+        
+        lineChartView.xAxis.labelPosition = .bottom
+        lineChartView.xAxis.labelTextColor = .systemGray3
+        lineChartView.xAxis.gridColor = .systemGray3
+        lineChartView.xAxis.valueFormatter = LineChartFormatter()
+        lineChartView.xAxis.drawAxisLineEnabled = false
+        lineChartView.xAxis.labelFont = .boldSystemFont(ofSize: 14)
+
+        lineChartView.leftAxis.enabled = false
+        lineChartView.rightAxis.labelTextColor = .systemGray3
+        lineChartView.rightAxis.gridColor = .systemGray3
+        lineChartView.rightAxis.labelFont = .boldSystemFont(ofSize: 14)
+
+        lineChartView.legend.enabled = false
+        
+        lineChartView.animate(xAxisDuration: 2, easingOption: .linear)
     }
 
     /*
@@ -39,4 +62,13 @@ class WeightViewController: UIViewController {
     }
     */
 
+}
+
+public class LineChartFormatter: NSObject, AxisValueFormatter{
+    let dayNames: [String]! = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri","Sat"]
+    
+    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        // 0 -> Jan, 1 -> Feb...
+        return dayNames[Int(value)]
+    }
 }
